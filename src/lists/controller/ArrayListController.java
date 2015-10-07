f71f54f9972a175af2da4245d639d6b03ea3cc07
Sydothekido11 <sydnemelka@gmail.com>
2015-10-07 13:55:08 -0600

    Added a loop and pop up method.

--- src/lists/controller/ArrayListController.java
+++ src/lists/controller/ArrayListController.java
@@ -1,18 +1,36 @@
 package lists.controller;
 
 import java.util.ArrayList;
-
+import lists.view.ListView;
 public class ArrayListController
 {
 	private ArrayList<String> firstWords;
-
+	private ListView popupDisplay;
+	
+	
 	public ArrayListController()
 	{
 		firstWords = new ArrayList<String>();
+		popupDisplay = new ListView();
 	}
+	/**
+	 * Displays a window that shows what the contents are at 0 and what the contents are when increased by 1, or each time okay is pressed in this case.
+	 */
 	
 	public void start()
 	{
 		firstWords.add("hsadusaioh");
+		firstWords.add("baaaaaaaahhhhhhbaaaaaahhhhbaaahhhhbwaaaahh");
+		displayList();
+	}
+	/**
+	 * Uses a loop for contents that increase by 1.
+	 */
+	private void displayList()
+	{
+		for(int spot = 0; spot < firstWords.size(); spot++)
+		{
+			popupDisplay.showList("The contents at " + spot + " are " + firstWords.get(spot));
+		}
 	}
 }
