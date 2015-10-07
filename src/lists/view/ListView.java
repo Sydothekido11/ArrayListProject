c6dce4f890c9409e5b8e87047abaf08610f3a6aa
Sydothekido11 <sydnemelka@gmail.com>
2015-10-07 13:31:00 -0600

    Added a viewer class and fixed the NPE.

---
+++ src/lists/view/ListView.java
@@ -0,0 +1,11 @@
+package lists.view;
+
+import javax.swing.JOptionPane;
+
+public class ListView
+{
+	public void showList(String currentWords)
+	{
+		JOptionPane.showMessageDialog(null, currentWords);
+	}
+}
